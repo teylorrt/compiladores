ced89a21a28875ed881cdb353f1ecfa9fd4e8467
Teylor Ribett Tavares <teylor.tavares@itixti.com.br>
2019-06-09 13:21:11 -0300

    ajuste tabela de simbolo

--- 1bim/trab/compiler.js
+++ 1bim/trab/compiler.js
@@ -1,20 +1,39 @@
 var fs = require("fs");
 var jison = require("jison");
 
+jison.escopo = 1;
+jison.tabelaSimbolo = [
+
+];
+
+jison.inserirSimbolo = function(lexema, token, categoria, tipo, valor, escopo, utilizada){
+    let simbolo = [];
+    simbolo['lexema'] = lexema;
+    simbolo['token'] = token;
+    simbolo['categoria'] = categoria;
+    simbolo['tipo'] = tipo;
+    simbolo['valor'] = valor;
+    simbolo['escopo'] = escopo;
+    simbolo['utilizada'] = utilizada;
+
+    jison.tabelaSimbolo.push(simbolo);
+};
+
 var grammar = JSON.parse(fs.readFileSync("json/grammar.json", "utf8"));
 grammar.lex = JSON.parse(fs.readFileSync("json/lex.json", "utf8"));
 grammar.bnf = JSON.parse(fs.readFileSync("json/bnf.json", "utf8"));
 grammar.operators = JSON.parse(fs.readFileSync("json/operators.json", "utf8"));
 
 var sourceSuccess = fs.readFileSync("sourceTestSuccess", "utf8");
-var sourceError = fs.readFileSync("sourceTestError", "utf8")
+var sourceError = fs.readFileSync("sourceTestError", "utf8");
 
 // Generate Compiler
 var compiler = new jison.Parser(grammar);
 
 var sintaticParser = function(src, srcName){
     try {
-        compiler.parse(src);
+        var teste = compiler.parse(src);
+        console.log(jison.escopo);
         console.log(srcName + ' is a valid code!');
     } catch (error) {
         console.log(srcName + ' is a invalid code:\n');
@@ -24,4 +43,4 @@ var sintaticParser = function(src, srcName){
 
 sintaticParser(sourceSuccess, "sourceTestSuccess");
 
-sintaticParser(sourceError, "sourceTestError");
+// sintaticParser(sourceError, "sourceTestError");
